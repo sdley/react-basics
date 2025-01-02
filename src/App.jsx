34d83bece026f573c22aa7647e99c7e6f0f0aa38
Souleymane DIALLO <sdley2020@gmail.com>
2025-01-02 11:03:21 +0000

    TailwindCSS config - initiation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
-import './App.css'
-
 function App() {
   
 
   return (
     <>
       <div>
-        
+        <h1 className="text-3xl font-bold text-red-500">App</h1>
       </div>
       
     </>
